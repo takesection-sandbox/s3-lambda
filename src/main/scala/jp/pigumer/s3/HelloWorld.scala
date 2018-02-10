06e6eeb2377135d3b2f7bfd87bb38bd5f87abe68
Shigeki Shoji <takesection@gmail.com>
2018-02-10 15:04:11 +0900

    edit handler

--- src/main/scala/jp/pigumer/s3/HelloWorld.scala
+++ src/main/scala/jp/pigumer/s3/HelloWorld.scala
@@ -1,10 +1,27 @@
 package jp.pigumer.s3
 
+import java.io.{IOException, InputStream, OutputStream}
+import java.nio.charset.StandardCharsets
+
 import com.amazonaws.services.lambda.runtime.Context
+import spray.json._
 
 class HelloWorld {
-  def handler(s3Event: Object, context: Context): Unit = {
+  @throws[IOException]
+  def handler(inputStream: InputStream, outputStream: OutputStream, context: Context): Unit = {
     val logger = context.getLogger
-    logger.log(s3Event.toString)
+    val json = {
+      try {
+        JsonParser(
+          new String(
+            Stream.continually(inputStream.read()).takeWhile(_ != -1).map(_.toByte).toArray,
+            StandardCharsets.UTF_8
+          )
+        )
+      } finally {
+        inputStream.close()
+      }
+    }
+    logger.log(json.compactPrint)
   }
 }
